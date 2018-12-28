84a684afdbf68b36e6f03aa9f54b822a56699025
Ricardo David Carrillo Sanchez <dominus.ceo@gmail.com>
2018-12-28 12:20:55 -0600

    Adding Linux fonts and powerline customization

--- script-enhance-f29.sh
+++ script-enhance-f29.sh
@@ -1,6 +1,7 @@
 #https://www.dedoimedo.com/computers/fedora-ultimate-customization-guide.html
 #https://opensource.com/article/17/8/customize-linux
-# https://itsfoss.com/best-gnome-extensions/
+#https://itsfoss.com/best-gnome-extensions/
+
 #Install script for enhance fedora
 USUARIO=ricardo.carrillo
 dnf update --refresh
@@ -12,7 +13,6 @@ systemctl enable sshd
 
 # Media Writer images called etcher-electron
 wget https://bintray.com/resin-io/redhat/rpm -O /etc/yum.repos.d/bintray-resin-io-redhat.repo
-
 
 ### Install chrome
 #https://www.if-not-true-then-false.com/2010/install-google-chrome-with-yum-on-fedora-red-hat-rhel/
@@ -35,7 +35,6 @@ dnf install -y http://download.virtualbox.org/virtualbox/rpm/fedora/26/x86_64/Vi
 /usr/lib/virtualbox/vboxdrv.sh setup
 usermod -a -G vboxusers $USUARIO
 
-
 #music players,codecs & video
 dnf config-manager --add-repo=http://negativo17.org/repos/fedora-spotify.repo
 dnf install -y quodlibet spotify-client vlc youtube-dl gnome-music smplayer smplayer-themes smtube soundconverter
@@ -48,6 +47,10 @@ dnf install -y gimp
 
 # Install Java plugins
 dnf install -y icedtea-web java-openjdk
+
+# Compilers for c-programming
+dnf install -y automake gcc gcc-c++ kernel-devel cmake python-devel
+dnf install -y ncurses-compat-libs
 
 # Communications
 dnf install -y pidgin pidgin-sipe pidgin-logviewer
@@ -88,8 +91,30 @@ dnf install -y keepass
 dnf install -y unzip p7zip
 
 # Windows packages for read NTFS partitions
-
 dnf install -y exfat-utils fuse-exfat
+
+
+# Powerline Install
+yum install -y vim-plugin-powerline powerline-fonts powerline vim-powerline
+cat << _EOF_ >> /root/.bashrc
+# User specific environment and startup programs
+if [ -f `which powerline-daemon` ]; then
+  powerline-daemon -q
+  POWERLINE_BASH_CONTINUATION=1
+  POWERLINE_BASH_SELECT=1
+  . /usr/share/powerline/bash/powerline.sh
+fi
+_EOF_
+
+cat << _EOF_ >> /home/$USUARIO/.bashrc
+# User specific environment and startup programs
+if [ -f `which powerline-daemon` ]; then
+  powerline-daemon -q
+  POWERLINE_BASH_CONTINUATION=1
+  POWERLINE_BASH_SELECT=1
+  . /usr/share/powerline/bash/powerline.sh
+fi
+_EOF_
 
 #software control version
 dnf install -y git
@@ -99,22 +124,18 @@ dnf group -y install --with-optional virtualization
 systemctl start libvirtd
 systemctl enable libvirtd
 
-### - revision #Vim customization
+### - Revision vim customization  - ###
 #https://robbinespu.github.io/eng/2018/05/17/My_Personal_Fedora28_setup.html
 #cd /tmp/; git clone https://github.com/tomasr/molokai.git
-#sudo dnf -y install vim vim-plugin-powerline
+dnf -y install vim
 #cp molokai/colors/molokai.vim /usr/share/vim/vim81/colors/
-#mkdir -pv ~/.vim/bundle
-#cd ~/.vim/bundle
-##git clone https://github.com/VundleVim/Vundle.vim.git
+#mkdir -pv ~/.vim/bundle && cd ~/.vim/bundle
+#git clone https://github.com/VundleVim/Vundle.vim.git
 #git clone https://github.com/Valloric/YouCompleteMe.git
-#dnf install -y automake gcc gcc-c++ kernel-devel cmake python-devel
 #cd ~/.vim/bundle/YouCompleteMe
 #git submodule update --init --recursive
 #/usr/bin/python ./install.py --clang-completer --go-completer --java-completer
 #vim +PluginInstall +qall
-#sudo dnf -y install ncurses-compat-libs
-
 
 #Activar extensiones gnome-extensions
 #descargar extensiones para navegador (firefox gnome-extensions)
@@ -146,12 +167,17 @@ dnf install -y gnome-shell-extension-panel-osd.noarch
 dnf install -y gnome-shell-extension-do-not-disturb-button.noarch
 dnf install -y gnome-shell-extension-system-monitor-applet.noarch
 
-
 #Investigar flathub|Flathub repositories
 #https://dl.flathub.org/repo/flathub.flatpakrepo
 # Herramienta como ccleaner - herramientas
 # Stacer
 #flatpak install flathub org.baedert.corebird
-# Instalacion de fuentes
-#sudo cp <Ubuntu fonts>.ttf /usr/share/fonts/
-#fc-cache -v -f
+
+# Red Hat and Ubuntu Fonts
+wget -c https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf -P /usr/share/fonts/PowerlineSymbols.otf
+wget -c https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf -P /etc/fonts/conf.d/10-powerline-symbols.conf
+cd /tmp && git clone https://github.com/RedHatBrand/Overpass.git
+cd Overpass && cp -rvap desktop-fonts/overpass* /usr/share/fonts/
+cd /tmp && git clone https://github.com/dominusceo/linux-fonts.git
+cd linux-fonts && cd Ubuntu cp -vap Ubuntu-*ttf /usr/share/fonts/
+fc-cache -v -f /usr/share/fonts/
